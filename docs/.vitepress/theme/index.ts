74e102a46870eb57a23e383f5bc20b594504bccb
Muggle <863457210@qq.com>
2024-12-13 16:13:09 +0800

    修改highlight引入方式

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -7,7 +7,7 @@ import TestUI from '@muggle-ui/components'
 
 import '@muggle-ui/theme-chalk/index.less'
 import 'highlight.js/styles/base16/summerfruit-light.css'
-import { component as VueHighlightJS } from '@highlightjs/vue-plugin'
+import hljsVuePlugin from '@highlightjs/vue-plugin'
 import Preview from './preview/index.vue'
 
 // 引入样式文件
@@ -22,6 +22,6 @@ export default {
   enhanceApp({ app, router, siteData }) {
     app.use(TestUI)
     app.component('preview', Preview)
-    app.use(VueHighlightJS)
+    app.use(hljsVuePlugin)
   },
 } satisfies Theme
